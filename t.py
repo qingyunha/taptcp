c7c032a7cc4339b00f9e534404ceef9922bec5c9
Tao Qingyun <845767657@qq.com>
2018-08-17 16:54:11 +0800

    icmp echo work. no cksum, no ip route

--- t.py
+++ t.py
@@ -1,7 +1,27 @@
 import os
 import struct
+import logging
 
 import tun
+
+class PK:
+
+    def __init__(self, b):
+        self.data = memoryview(b)
+        self.len = len(b)
+
+    def eth(self):
+        return eth_hdr.unpack(self.data[:eth_hdr.size])
+
+    def arpp(self):
+        return arp.unpack(self.data[eth_hdr.size:eth_hdr.size+arp.size])
+
+    def ip(self):
+        return IP.parse(self.data[eth_hdr.size:])
+
+    def icmp(self):
+        return ICMP.parse(self.ip().data)
+
 
 
 eth_hdr = struct.Struct(">6s6sH")
@@ -28,42 +48,141 @@ ARP_OP_REPLY = 2
 arp_cache = {} # ip --> mac
 
 
+def ip2s(ip):
+    return ".".join(str(b) for b in ip.to_bytes(4, "big"))
+
+class IP:
+
+    hdr = struct.Struct(">BBHHHBBHII")
+    IP_P_ICMP = 1
+    IP_P_TCP = 6
+    IP_P_UDP = 17
+    IP_P_RAW = 255
+
+    ID = [0]
+  
+    def __init__(self):
+        self.ver = 4
+        self.hlen = 5
+        self.tos = 0
+        self.len = 0
+        self.id = self.ID[0]; self.ID[0] += 1
+        self.fragoff = 0
+        self.ttl = 64
+        self.pro = 0
+        self.cksum = 0
+        self.src = 0
+        self.dst = 0
+        self.data = b''
+
+    @classmethod
+    def parse(cls, pk):
+        self = cls()
+        self.ver = pk[0] >> 4
+        self.hlen = pk[0] & 0x0f
+        if self.ver != 4:
+            raise Exception("only handle IPv4 %s (%d %d)", bin(pk[0]),self.hlen, self.ver)
+        self.tos, self.len, self.id, self.fragoff, \
+        self.ttl, self.pro, self.cksum, self.src, \
+        self.dst = cls.hdr.unpack(pk[:cls.hdr.size])[1:]
+        self.data = pk[cls.hdr.size:]
+        return self
+
+    def pack(self):
+        hdr = self.hdr.pack(self.ver<<4|self.hlen, self.tos, self.len, self.id, self.fragoff,
+                self.ttl, self.pro, self.cksum, self.src, self.dst)
+        return hdr + self.data
+
+
+    def __str__(self):
+        temp = """\
+------------------------------------------
+| %d  | %d  |         |       %d         |
+------------------------------------------
+|                     |                  |
+------------------------------------------
+|    %d     |    %d   |                  |
+------------------------------------------
+|                   %s              |
+------------------------------------------
+|                   %s              |
+------------------------------------------
+"""
+        return temp % (self.ver, self.hlen, self.len, self.ttl, self.pro,
+            ip2s(self.src), ip2s(self.dst))
+
+
+class ICMP:
+
+    hdl = struct.Struct(">BBH")
+    ping = struct.Struct(">HH")
+  
+    ICMP_T_ECHORLY = 0
+    ICMP_T_ECHOREQ = 8
+
+    def __init__(self):
+        self.type = 0
+        self.code = 0
+        self.chksum = 0
+        self.id = 0
+        self.seq = 0
+        self.data = 0
+
+    @classmethod
+    def parse(cls, pk):
+        self = cls()
+        hs = cls.hdl.size
+        self.type, self.code, self.cksum = cls.hdl.unpack(pk[:hs])
+        if self.type != cls.ICMP_T_ECHOREQ:
+            raise Exception("Only support echo request. %s", self.type)
+        self.id, self.seq = cls.ping.unpack(pk[hs:hs+cls.ping.size])
+        self.data = pk[hs+cls.ping.size:]
+        return self
+
+    def reply_pk(self):
+        pk = self.hdl.pack(self.ICMP_T_ECHORLY, 0, 0)
+        pk += self.ping.pack(self.id, self.seq)
+        pk += bytes(self.data)
+        return pk
+
+    def __str__(self):
+        return "id:%d  seq:%d" % (self.id, self.seq)
+      
 
 fd = tun.open_tap("tap1")
 os.system("ip link set tap1 up")
 os.system("ip address add 192.168.1.11/24 dev tap1")
 
 my_netdev = {
-    "ipaddr": int.from_bytes(bytes([192, 168, 1, 1]), "big"),
+    "ipaddr": int.from_bytes(bytes([192, 168, 1, 2]), "big"),
     "hwaddr": b"\x00\x34\x45\x67\x89\xab",
 }
 
 
 def pack_recv():
-    pack = os.read(fd, 1500)
+    pk = PK(os.read(fd, 1500))
     try:
-        dmac, smac, eth_type = eth_hdr.unpack(pack[:eth_hdr.size])
+        dmac, smac, eth_type = pk.eth()
         print("dmac:%s smac:%s type:%X(%s) len:%d" % (
             pmac(dmac), pmac(smac),
             eth_type,
             eth_p_map.get(eth_type),
-            len(pack)))
+            pk.len))
         _type = eth_p_map.get(eth_type)
         if _type == "ARP":
-            arp_in(pack[eth_hdr.size:])
+            arp_in(pk)
         elif _type == "IP":
-            ip_in(pack[eth_hdr.size:])
+            ip_in(pk)
         else:
-            print("no support packet, drop")
+            "no support packet, drop"
     except struct.error as e:
         print("unpack eth_hdr error %s" % e)
-    return pack[eth_hdr.size:]
 
 
 def arp_in(pk):
     print("process arp")
     try:
-        hwtype, protype, _, _, opcode, smac, sip, dmac, dip = arp.unpack(pk)
+        hwtype, protype, _, _, opcode, smac, sip, dmac, dip = pk.arpp()
     except struct.error as e:
         print("unpack apr error %s. drop" % e)
         return
@@ -80,7 +199,7 @@ def arp_in(pk):
         print("arp not for us %s. drop", dip)
         return
 
-    print("update arp_cache")
+    print("update arp_cache", ip2s(sip), pmac(smac))
     arp_cache[sip] = smac
 
     if opcode == ARP_OP_REQUEST:
@@ -100,7 +219,40 @@ def netdev_tx(pk, proto, dmac):
 
 
 def ip_in(pk):
-   print("process ip")
+    print("process ip")
+    try:
+        ip = pk.ip()
+    except Exception as e:
+        print("parse ip packet error %s" % e)
+        return
+    if ip.dst != my_netdev["ipaddr"]:
+        print("ip not for us %s. drop" % ip2s(ip.dst))
+        return
+    if ip.pro == IP.IP_P_ICMP:
+        icmp_in(pk)
+
+
+def icmp_in(pk):
+    print("process icmp")
+    ip = pk.ip()
+    icmp = pk.icmp()
+    print(icmp)
+    data = icmp.reply_pk()
+    dst = ip.src
+    ip_out(ip, data, dst)
+
+
+def ip_out(orig_ip, data, dst):
+    print("send ip out")
+    ip = orig_ip
+    ip.src = my_netdev["ipaddr"]
+    ip.dst = dst
+    ip.data = data
+    dmac = arp_cache.get(ip.dst)
+    if not dmac:
+        print("can find dmac", ip.dst, arp_cache)
+        return
+    netdev_tx(ip.pack(), ETH_P_IP, dmac)
 
 
 if __name__ ==  '__main__':
